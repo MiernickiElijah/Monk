e4f400f1adbcc78418b3020d5dc91cd1910e3b0f
Elijah Miernicki <miernickielijah@gmail.com>
2021-07-13 17:31:57 -0500

    added ERD for the index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,31 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    module: ProductTag,
+    unique: false
+  }
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    module: ProductTag,
+    unique: false
+  }
+});
 
 module.exports = {
   Product,
