acd7f8a199691fe2c47e21e104f9cc04f3968f7e
Elijah Miernicki <miernickielijah@gmail.com>
2021-07-15 18:49:35 -0500

    updated get routes with simpler code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,64 +1,29 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Category, Product } = require('../../models');
+const { Category, Product, ProductTag, Tag } = require('../../models');
 
 // The `/api/categories` endpoint
 
+// find all categories
+// be sure to include its associated Products
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
-  try {
-    const catData = await Category.findAll({
-      include: [{ model: Category }, { model: Product }],
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              '(SELECT COUNT(*) FROM category AND product = id)'
-            ),
-            'product'
-          ],
-        ],
-      },
-    });
-    res.status(200).json(catData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Category.findAll({ include: [Product] })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
 
 // find one category by its `id` value
 // be sure to include its associated Products
 router.get('/:id', async (req, res) => {
-  try {
-    const singleCat = await Category.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Product }],
-      attributes: {
-        include: [
-          [
-            // Use plain SQL to get a count of all short books
-            sequelize.literal(
-              '(SELECT COUNT(*) FROM category AND product = id)'
-            ),
-            'shortBooks',
-          ],
-        ],
-      },
-    });
-    if (!singleCat) {
-      res.status(404).json({ message: 'No Category found with that id!' });
-      return;
-    }
-    res.status(200).json(singleCat);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Category.findOne({ where: { id: req.params.id }, include: [Product] })
+    .then((singleCatData) => res.json(singleCatData))
+    .catch((err) => res.status(400).json(err));
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
   try {
-    const newCatData = await Category.create({
+    const newCatData = Category.create({
       product_id: req.body.product_id,
     });
     res.status(200).json(newCatData);
@@ -67,10 +32,10 @@ router.post('/', (req, res) => {
   }
 });
 
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
   try {
-    const updateCatData = await Category.put({
+    const updateCatData = Category.put({
       product_id: req.body.product_id,
     });
     res.status(200).json(updateCatData);
@@ -79,10 +44,10 @@ router.put('/:id', (req, res) => {
   }
 });
 
+// delete a category by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
   try {
-    const catData = await Category.destroy({
+    const catData = Category.destroy({
       where: { id: req.params.id }
     });
     if (!catData) {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,65 +2,29 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
+// find all tags
+// be sure to include its associated Product data
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
-  try {
-    const tagData = await Tag.findAll({
-      include: [{ model: ProductTag }, { model: Product }],
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              '(SELECT COUNT(*) FROM category AND product = id)'
-            ),
-            'product'
-          ],
-        ],
-      },
-    });
-    res.status(200).json(tagData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Tag.findAll({ where: { id: req.params.id }, include: [Product] })
+    .then((tags) => res.json(tags))
+    .catch((err) => res.status(400).json(err));
 });
 
+// find a single tag by its `id`
+// be sure to include its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
-  try {
-    const singleTag = await Tag.findByPk(req.params.id, {
-      include: [{ model: ProductTag }, { model: Product }],
-      attributes: {
-        include: [
-          [
-            // Use plain SQL to get a count of all short books
-            sequelize.literal(
-              '(SELECT COUNT(*) FROM category AND product = id)'
-            ),
-            'product',
-          ],
-        ],
-      },
-    });
-    if (!singleTag) {
-      res.status(404).json({ message: 'No tag found with that id!' });
-      return;
-    }
-    res.status(200).json(singleTag);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  Tag.findOne({ where: { id: req.params.id }, include: [Product] })
+    .then((singleTagData) => res.json(singleTagData))
+    .catch((err) => res.status(400).json(err));
 });
 
 router.post('/', (req, res) => {
   // create a new tag
   try {
-    const newCatData = await Tag.create({
+    const newTagData = Tag.create({
       product_id: req.body.product_id,
     });
-    res.status(200).json(newCatData);
+    res.status(200).json(newTagData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -69,7 +33,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const updateTagData = await Tag.put({
+    const updateTagData = Tag.put({
       product_id: req.body.product_id,
     });
     res.status(200).json(updateTagData);
@@ -81,7 +45,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = await Tag.destroy({
+    const tagData = Tag.destroy({
       where: { id: req.params.id }
     });
     if (!tagData) {
